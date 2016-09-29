b0a2a0b03472637adbc3c3cfc36171940252cb30
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-09-29 19:37:10 +1300

    Created level select screen, added scripts to handle grid resizing and next/previous pages

---
+++ Assets/Scripts/LevelGrids.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelGrids : MonoBehaviour {
+
+	public int col, row;
+
+	// Use this for initialization
+	void Start () {
+
+		RectTransform parent = gameObject.GetComponent<RectTransform> ();
+		GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup> ();
+
+		grid.cellSize = new Vector2 (parent.rect.width / col, parent.rect.height / row);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RectTransform parent = gameObject.GetComponent<RectTransform> ();
+		GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup> ();
+
+		grid.cellSize = new Vector2 (parent.rect.width / col, parent.rect.height / row);
+	
+	}
+}
