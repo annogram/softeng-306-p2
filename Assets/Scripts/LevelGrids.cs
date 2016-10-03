5244610d393f681e3e4113d9b50c49e4226bcb08
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-04 12:49:43 +1300

    Changed settings to handle binary files

--- Assets/Scripts/LevelGrids.cs
+++ Assets/Scripts/LevelGrids.cs
@@ -9,11 +9,6 @@ public class LevelGrids : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		RectTransform parent = gameObject.GetComponent<RectTransform> ();
-		GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup> ();
-
-		grid.cellSize = new Vector2 (parent.rect.width / col, parent.rect.height / row);
-
 	}
 	
 	// Update is called once per frame
