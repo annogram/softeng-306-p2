6a25c5176c72690a4f4446c20e1adc4c31b1d139
Jay Gradon <jaygradon@outlook.com>
2016-10-03 15:27:23 +1300

    Removed Library files, fixed jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,8 +21,6 @@ public class PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start() {
         _ball = false;
-        _currentAccl = accl;
-        _currentMaxSpeed = maxSpeed;
         _rb = GetComponent<Rigidbody2D>();
         _feet = GetComponent<EdgeCollider2D>();
     }
