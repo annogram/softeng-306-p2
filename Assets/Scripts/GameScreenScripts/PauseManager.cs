7daae9597de54710876145ba591013c6804be580
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-05 14:52:43 +1300

    Pause functionality working on level1 scene

---
+++ Assets/Scripts/GameScreenScripts/PauseManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour {
+	public GameObject pausePanel;
+	// Use this for initialization
+	void Start () {
+		//Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			pausePanel.SetActive (!pausePanel.active);
+			Time.timeScale = 0.0f;
+		}
+	
+	}
+
+	public void resume () {
+		pausePanel.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+		
+		
+}
