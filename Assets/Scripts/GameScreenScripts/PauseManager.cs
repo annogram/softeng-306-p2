a39ac247d1d48e0c6f5b37f71d3990c46487df16
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-05 17:20:10 +1300

    Added blur effect on main camera for pause, imported Standard Assets to use the Blur image effect, camera background for testing purposes

--- Assets/Scripts/GameScreenScripts/PauseManager.cs
+++ Assets/Scripts/GameScreenScripts/PauseManager.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 public class PauseManager : MonoBehaviour {
 	public GameObject pausePanel;
+	public Camera cam;
 	// Use this for initialization
 	void Start () {
 		//Time.timeScale = 1;
@@ -12,14 +14,19 @@ public class PauseManager : MonoBehaviour {
 	void Update () {
 
 		if (Input.GetKeyDown (KeyCode.P)) {
-			pausePanel.SetActive (!pausePanel.active);
-			Time.timeScale = 0.0f;
+			if (!pausePanel.active) {
+				pausePanel.SetActive (true);
+				cam.GetComponent<Blur>().enabled = true;
+				Time.timeScale = 0.0f;
+			} else {
+				resume ();
+			}
 		}
-	
 	}
 
 	public void resume () {
 		pausePanel.SetActive (false);
+		cam.GetComponent<Blur>().enabled = false;
 		Time.timeScale = 1.0f;
 	}
 		
