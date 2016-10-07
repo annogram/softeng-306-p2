7dcbbb5edea960aea006f39b920d4804f20f2def
Akram <akrdar@gmail.com>
2016-10-08 10:55:03 +1300

    Making the game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    private static GameController _instance;
+
+    void Awake() {
+        if (_instance == null) {
+            _instance = this;
+        } 
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    #region Helper methods
+    #endregion
+}
