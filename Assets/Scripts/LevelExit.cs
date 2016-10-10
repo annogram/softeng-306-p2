b87210ec994b7148f60f44ec780f4287bc443f56
Akram <akrdar@gmail.com>
2016-10-10 23:16:40 +1300

    Added check for player before terminating the level

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -10,8 +10,26 @@ using Managers;
 public class LevelExit : MonoBehaviour {
     public string nextScene;
 
+    private bool Playa1IsInDaHouse = false;
+    private bool Playa2IsInDaHouse = false;
+
     void OnTriggerEnter2D(Collider2D other) {
-        GameController controller = GameController.Instance;
-        controller.loadScreenSingle(nextScene);
+        if (other.tag == "Player") {
+            Playa1IsInDaHouse = true;
+        }else if(other.tag == "Player2") {
+            Playa2IsInDaHouse = true;
+        }
+        if (Playa1IsInDaHouse && Playa2IsInDaHouse) {
+            GameController controller = GameController.Instance;
+            controller.loadScreenSingle(nextScene);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Playa1IsInDaHouse = false;
+        } else if (other.tag == "Player2") {
+            Playa2IsInDaHouse = false;
+        }
     }
 }
