aaf4bc83fdaabdc05d6e34b06952c7ad536ea1db
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-12 19:02:31 +1300

    Hooked in about page to start page through script

---
+++ Assets/Scripts/GameScreenScripts/AboutPage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AboutPage : MonoBehaviour {
+	public GameObject start;
+	public GameObject about;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void togglePage(){
+		start.SetActive (!start.active);
+		about.SetActive (!about.active);
+
+	}
+
+}
