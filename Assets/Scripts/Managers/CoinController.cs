b1a51f7254c5747e7b4221f1d172f41164971198
Taranpreet Kohli <taranpreetkohli@yahoo.co.nz>
2016-10-14 21:07:20 +1300

    Coin test and Coin controller added

---
+++ Assets/Scripts/Managers/CoinController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Managers{
+
+	public class CoinController : MonoBehaviour {
+
+		private GameController _gameController;
+
+		void Start(){
+			_gameController = GameController.Instance;
+		}
+
+		void OnTriggerEnter2D(Collider2D other){
+			if (other.tag == "Player" || other.tag == "Player2") {
+				CoinPickup ();
+			}
+		}
+
+		void CoinPickup(){
+			_gameController.AddToken ();
+			Destroy (this.gameObject);
+		}
+			
+
+	}
+
+}
