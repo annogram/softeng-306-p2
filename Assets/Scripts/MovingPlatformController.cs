e76ffebc94d97d53596b66a0507b40de1d13703e
Helen Zhao <hzha587@aucklanduni.ac.nz>
2016-10-15 17:53:36 +1300

    Moving platforms implemented

--- Assets/Scripts/MovingPlatformController.cs
+++ Assets/Scripts/MovingPlatformController.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class MovingPlatformController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private int baseValue = 0;
+    private Vector3 moveDown = new Vector3(0, -0.3F, 0);
+    private Vector3 moveUp = new Vector3(0, 0.3F, 0);
+    private Vector3 move = new Vector3(0, 0.3F, 0);
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this.transform.position += move;
+	    if(this.transform.position.y > 10)
+        {
+            move = moveDown;
+        } 
+        if(this.transform.position.y < -10)
+        {
+            move = moveUp;
+        }
 	}
 }
