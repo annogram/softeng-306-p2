11a3a44fbff5a94df8fc93c3608802c5a96e0e32
taranpreetkohli <tkoh638@aucklanduni.ac.nz>
2016-10-17 12:30:32 +1300

    Teleporter script added - not yet tested

---
+++ Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterScript : MonoBehaviour {
+
+	public GameObject teleportDestination;
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Teleporter touched.");
+		if (other.tag != "Player" || other.tag != "Player2") {
+			return;
+		}
+
+		other.transform.position = teleportDestination.transform.position;
+	}
+}
