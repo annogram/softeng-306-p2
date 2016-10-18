75afc4897f854bc8c58cc4be9a4dd24d2d40a50d
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-19 12:11:03 +1300

    Create team on new game - login feature makes call to game controller to search for existing team names

--- Assets/Scripts/Managers/LoginManager.cs
+++ Assets/Scripts/Managers/LoginManager.cs
@@ -12,15 +12,23 @@ namespace Managers {
 		private const string NEXT_SCENE_FOLLOWING_LOGIN = "AvatarSelectScreen";
 
 		private GameController instance;
+
+		private Text messageText;
 	
 		void Start () {
 			instance = GameController.Instance;
+			Transform message = this.gameObject.transform.Find ("Message");
+			messageText = message.GetComponent<Text> ();
 		}
 
-		void NewGame() {
+		public void NewGame() {
 			bool attempt = instance.attemptTeamNewGame (inputField.text);
 			if (attempt) {
-				instance.loadScreenSingle(NEXT_SCENE_FOLLOWING_LOGIN);
+				instance.loadScreenSingle (NEXT_SCENE_FOLLOWING_LOGIN);
+				messageText.text = "";
+			} else {
+				Transform message = this.gameObject.transform.Find ("Message");
+				messageText.text = "Team already exists! Can't create new game.";
 			}
 		}
 
