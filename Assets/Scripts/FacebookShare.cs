97b40be13f69ce772df570093e1d3de71d6882cc
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-10-18 18:24:19 +1300

    FB share script now takes in game score data, removed hard coded scores

--- Assets/Scripts/FacebookShare.cs
+++ Assets/Scripts/FacebookShare.cs
@@ -1,39 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Managers;
+using UnityEngine.EventSystems;
 
+/* Script which handlesFacebook posting. To be attatched to a button, and used by invoking 
+ * the ShareScoreOnFB method */
 public class FacebookShare : MonoBehaviour {
-	// Your app’s unique identifier.
+	// App’s unique identifier.
 	public string AppID = "1399925576886522";
 
 	// The link attached to this post.
 	public string Link = "https://play.google.com/store/apps/developer?id=Gamestoodio";
 
 	// The URL of a picture attached to this post. The picture must be at least 200px by 200px.
-	public string Picture = "http://imageshack.us/scaled/landing/843/gh4o.png";
+	public string Picture = "http://i65.tinypic.com/35hiigw.jpg";
 
 	// The name of the link attachment.
-	public string Name = "My New Score";
+	public string Name = "PINSCAPE";
 
 	// The caption of the link (appears beneath the link name).
-	public string Caption = "I just got +99 score friends! Can you beat it?";
+	public string Caption = "SuicideSquad";
 
 	// The description of the link (appears beneath the link caption).
-	public string Description = "Enjoy fun, free games! Challenge yourself or share with friends. Fun and easy-to-use game.";
-
-	void OnGUI () {
-		float x = (Screen.width/2)-50;
-		float y = (Screen.height/2)-25;
-
-		if(GUI.Button(new Rect( x, y, 100, 50), "Share Score")) {
-			ShareScoreOnFB(); 
-		} 
-	}
+	public string Description = "My High Score on Pinscape is 100! Can you beat it?";
 
 	public void ShareScoreOnFB(){
+		string score = GameController.Instance.GetTotalTokens ().ToString ();
+		Caption = "My High Score on Pinscape is " + score + "! Can you beat it?";
 		Application.OpenURL("https://www.facebook.com/dialog/feed?"+ "app_id="+AppID+ "&link="+
 			Link+ "&picture="+Picture+ "&name="+ReplaceSpace(Name)+ "&caption="+
 			ReplaceSpace(Caption)+ "&description="+ReplaceSpace(Description)+
 			"&redirect_uri=https://facebook.com/");
+
+		EventSystem.current.SetSelectedGameObject(null);
 	}
 
 	string ReplaceSpace (string val) {
