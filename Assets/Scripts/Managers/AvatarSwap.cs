bb6e07466a7013a3ced7cb36e58855a9589ef4e4
taranpreetkohli <tkoh638@aucklanduni.ac.nz>
2016-10-18 15:27:53 +1300

    AvatarSwap script created

---
+++ Assets/Scripts/Managers/AvatarSwap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Managers {
+	public class AvatarSwap : MonoBehaviour {
+
+		private GameController controller;
+
+
+		void Start() {
+			this.controller = GameController.Instance;
+		}
+			
+		public void NextPlayerOne() {
+			// toggle to next avatar
+		}
+			
+		public void PreviousPlayerOne(){
+			// go back to previous avatar
+		}
+
+		public void NextPlayerTwo() {
+
+
+		}
+
+		public void PreviousPlayerTwo(){
+
+		}
+	}
+}
