8203ee481d4746ff57d21ca208d3ad36a3aea57f
taranpreetkohli <tkoh638@aucklanduni.ac.nz>
2016-10-19 14:42:03 +1300

    Highscore screen logged in check script implementation - partial

---
+++ Assets/Scripts/GameScreenScripts/HighscoreScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Managers;
+
+public class HighscoreScreen : MonoBehaviour {
+
+	public GameObject highscreenPopup;
+
+	private GameController instance;
+
+	void Start () {
+		instance = GameController.Instance;
+	}
+	
+	void HighscoreScreenRequested() {
+
+		if (instance.LoggedIn) {
+			instance.loadScreenSingle ("HSScreen");
+		} else {
+			// show popup
+		}
+
+	}
+}
