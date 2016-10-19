315ec5b3adea6a3ad102c9c9eef7bf65a72c548a
gtio848 <gtio848@aucklanduni.ac.nz>
2016-10-19 18:39:35 +1300

    Implemented colour selection on AvatarSelectScreen.

--- Assets/Scripts/Managers/AvatarSwap.cs
+++ Assets/Scripts/Managers/AvatarSwap.cs
@@ -13,27 +13,55 @@ public enum SkinColour {
 namespace Managers {
 	public class AvatarSwap : MonoBehaviour {
 
-		private GameController controller;
-		private SkinColour player1;
-		private SkinColour player2;
+        public Sprite BlueIdle;
+        public Sprite GreenIdle;
+        public Sprite RedIdle;
+        public Sprite PurpleIdle;
+
+        private GameController controller;
+		private SkinColour _player1Colour;
+		private SkinColour _player2Colour;
+        private GameObject _player1Sphere;
+        private GameObject _player2Sphere;
 
 		void Start() {
 			this.controller = GameController.Instance;
-		}
+            _player1Sphere = GameObject.Find("Player1Sphere");
+            _player2Sphere = GameObject.Find("Player2Sphere");
+            _player1Colour = SkinColour.BLUE;
+            _player2Colour = SkinColour.RED;
+        }
 			
 		public void TogglePlayerOne() {
-            // TODO change the player image in scene
-			player1 = player1 == SkinColour.BLUE ? SkinColour.GREEN : SkinColour.BLUE;
+            if (_player1Colour == SkinColour.BLUE)
+            {
+                _player1Sphere.GetComponent<SpriteRenderer>().sprite = GreenIdle;
+                _player1Colour = SkinColour.GREEN;
+            }
+            else
+            {
+                _player1Sphere.GetComponent<SpriteRenderer>().sprite = BlueIdle;
+                _player1Colour = SkinColour.BLUE;
+            }
+            
 		}
 
 		public void TogglePlayerTwo() {
-            // TODO change the player image in scene
-            player2 = player2 == SkinColour.RED ? SkinColour.PURPLE : SkinColour.RED;
-		}
+            if (_player2Colour == SkinColour.RED)
+            {
+                _player2Sphere.GetComponent<SpriteRenderer>().sprite = PurpleIdle;
+                _player2Colour = SkinColour.PURPLE;
+            }
+            else
+            {
+                _player2Sphere.GetComponent<SpriteRenderer>().sprite = RedIdle;
+                _player2Colour = SkinColour.RED;
+            }
+        }
 			
 
 		public void SubmitSkinsToController(){
-			controller.SaveAvatarSelection (player1, player2);
+			controller.SaveAvatarSelection (_player1Colour, _player2Colour);
 		}
 			
 	}
