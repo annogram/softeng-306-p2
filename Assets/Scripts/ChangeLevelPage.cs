e9461f3437116d6c363650123354dd10863e9e7e
Michael <kim.michael95@gmail.com>
2016-10-20 15:55:33 +1300

    Commented some scripts

--- Assets/Scripts/ChangeLevelPage.cs
+++ Assets/Scripts/ChangeLevelPage.cs
@@ -1,25 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+///<summary>
+/// This class is responsible for dealing with chaning the level selection page
+///</summary>
 public class ChangeLevelPage : MonoBehaviour {
 
 	public GameObject pageOne = null;
 	public GameObject pageTwo = null;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-	}
-
+	// Changes to next page
 	public void nextPage(){
-
 		pageOne.SetActive (!pageOne.active);
 		pageTwo.SetActive (!pageTwo.active);
-
-	
 	}
 }
